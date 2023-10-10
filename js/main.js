a211a98eb131bd85bacf1c95c6dc30e720216efb
larissamiilena <107041363+larissamiilena@users.noreply.github.com>
2023-10-09 22:28:27 -0300

    0910

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("sou linda")
+console.log("jogue medocument.querySelector("#subtrair")
+const subtrair = document.querySelector("#subitrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+somar.addEventListener("click", => () {
+ console.log(parceIntvaco.value += 1))
+})
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1;
+})
