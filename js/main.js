ec6a9d96263f5782f0eaa8fb1f463030b39144f1
larissamiilena <107041363+larissamiilena@users.noreply.github.com>
2023-10-16 21:18:08 -0300

    oi

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const robotron = document.querySelector("#robotron")
 robotron.addEventListener("click", dizOi)
 
 function dizOi() {
-    console.log('Oi')
+    console.log("Oi")
     console.log("Bem-vindo ao Robotron 2000")
 }
 dizOi()
